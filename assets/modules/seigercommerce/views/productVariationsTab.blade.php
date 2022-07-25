6f42a56a0d1419f04a0e31c705d2060fedeaaa78
Serhii Kornelyuk <seiger@meta.ua>
2022-07-25 23:50:17 +0300

    [FIX] Optional products variations tab.

--- assets/modules/seigercommerce/views/productVariationsTab.blade.php
+++ assets/modules/seigercommerce/views/productVariationsTab.blade.php
@@ -3,14 +3,14 @@
 <div class="row form-row">
     <div class="row-col col-lg-6 col-md-6 col-12">
         <div class="row form-row">
-            <div class="col-auto col-title">
+            <div class="col-auto">
                 <label for="grouping_parameters" class="warning">{{$_lang["scommerce_grouping_parameter"]}}</label>
                 <i class="fa fa-question-circle" data-tooltip="{{$_lang['scommerce_grouping_parameter_help']}}"></i>
             </div>
             <div class="col">
                 <select id="grouping_parameters" class="form-control" name="grouping_parameters[]" onchange="changeVariations();">
                     @foreach($sCommerce->getProductOptionalsSelect((int)request()->i) as $optionalsSelect)
-                        <option value="{{$optionalsSelect->id}}" @if(in_array($optionalsSelect->id, $product->grouping_parameters_array)) selected @endif>{{$optionalsSelect->pagetitle}}</option>
+                        <option value="{{$optionalsSelect->filter}}" @if(in_array($optionalsSelect->filter, $product->grouping_parameters_array)) selected @endif>{{$optionalsSelect->pagetitle}}</option>
                     @endforeach
                 </select>
             </div>
@@ -26,26 +26,34 @@
         <tr>
             <th style="text-align:center;">{{$_lang["scommerce_variation"]}}</th>
             <th style="text-align:center;">{{$_lang["scommerce_price"]}}</th>
+            <th style="text-align:center;">{{$_lang["scommerce_code"]}}</th>
             <th style="text-align:center;">{{$_lang["resource_opt_published"]}}</th>
         </tr>
         </thead>
         <tbody>
         @foreach($product->grouping_parameters_array as $filterId)
-            @foreach($sCommerce->filterValues($filterId) as $value)
-                <tr>
-                    <td>{{$value->base}}</td>
-                    <td>
-                        <div class="input-group">
-                            <span class="input-group-text">₴</span>
-                            <input type="text" name="variations[{{$value->vid}}][price]" class="form-control" value="{{$product->variations_array[$value->vid]['price'] ?? $product->price}}" onchange="documentDirty=true;">
-                        </div>
-                    </td>
-                    <td>
-                        <input type="hidden" name="variations[{{$value->vid}}][published]" class="form-control" value="0">
-                        <input type="checkbox" name="variations[{{$value->vid}}][published]" class="form-checkbox form-control" value="1" @if(isset($product->variations_array[$value->vid]['published']) && $product->variations_array[$value->vid]['published']) checked @endif>
-                    </td>
-                </tr>
-            @endforeach
+            @if((int)$filterId)
+                @foreach($sCommerce->filterValues($filterId) as $value)
+                    <tr>
+                        <td>{{$value->base}}</td>
+                        <td>
+                            <div class="input-group">
+                                <span class="input-group-text">€</span>
+                                <input type="text" name="variations[{{$value->vid}}][price]" class="form-control" value="{{$product->variations_array[$value->vid]['price'] ?? $product->price}}" onchange="documentDirty=true;">
+                            </div>
+                        </td>
+                        <td>
+                            <div class="input-group">
+                                <input type="text" name="variations[{{$value->vid}}][code]" class="form-control" value="{{$product->variations_array[$value->vid]['code'] ?? $product->code}}" onchange="documentDirty=true;">
+                            </div>
+                        </td>
+                        <td>
+                            <input type="hidden" name="variations[{{$value->vid}}][published]" value="0">
+                            <input type="checkbox" name="variations[{{$value->vid}}][published]" class="form-checkbox form-control" value="1" @if(isset($product->variations_array[$value->vid]['published']) && $product->variations_array[$value->vid]['published']) checked @endif>
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
         @endforeach
         </tbody>
     </table>
